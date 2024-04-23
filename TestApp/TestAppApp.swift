11667da6306068aa854ade9528854a8b5fd1f2ff
gunna-rahul <grk25303@gmail.com>
2024-04-23 13:26:25 +0530

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by admin on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
